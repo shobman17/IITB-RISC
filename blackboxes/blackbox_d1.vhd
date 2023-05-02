2efad6f128f475cc5fea899a840f7c2207829222
Abhiruchi Kotamkar <kabhiruchi@gmail.com>
2023-05-03 02:05:42 +0530

    adding blackboxes

---
+++ blackboxes/blackbox_d1.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library Work;
+entity bbD1 is
+	port (
+			mux_rf_a1_output, or2ex_a3, ex2ma_a3, ma2wb_a3, or2ex_rf_wr, ex2ma_rf_wr, ma2wb_rf_wr:in std_logic;
+		   id2or_mux_alu_a : in std_logic_vector(1 downto 0);
+			mux_rf_d1_1, mux_rf_d1_0 : out std_logic);
+end entity bbD1;
+
+architecture blackboxed of bbD1 is
+	--signal c, z: std_logic := '0';
+	--signal storage: std_logic_vector(1 downto 0):="00";
+	begin
+		edit_process: process(mux_rf_a1_output, or2ex_a3, ex2ma_a3, ma2wb_a3, or2ex_rf_wr, ex2ma_rf_wr, ma2wb_rf_wr,id2or_mux_alu_a)
+		begin
+			if (mux_rf_a1_output = or2ex_a3 and or2ex_rf_wr = '1' and id2or_mux_alu_a = "00") then
+			   mux_rf_d1_1<='0';
+				mux_rf_d1_0<='1';
+			elsif (mux_rf_a1_output = ex2ma_a3 and ex2ma_rf_wr ='1' and id2or_mux_alu_a = "00") then
+			   mux_rf_d1_1<='1';
+				mux_rf_d1_0<='0';
+			elsif (mux_rf_a1_output = ma2wb_a3 and ma2wb_rf_wr = '1'and id2or_mux_alu_a = "00") then
+			   mux_rf_d1_1<='1';
+				mux_rf_d1_0<='1';
+			else 
+				mux_rf_d1_1<='0';
+				mux_rf_d1_0<='0';
+			end if;
+			
+		end process;
+end architecture blackboxed;
---
+++ blackboxes/blackbox_d3.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library Work;
+entity bbD3 is
+	port (
+			ex2ma_c, ex2ma_z:in std_logic;
+		   opcode : in std_logic_vector(5 downto 0);
+			c_wr, z_wr : out std_logic);
+end entity bbD3;
+
+architecture blackboxed3 of bbD3 is
+	begin
+		edit_process: process(ex2ma_c, ex2ma_z, opcode)
+		begin
+			if (opcode = "000100" or opcode = "000111" or opcode = "000000" or opcode = "000001" or opcode = "000010" or opcode = "000011") then
+			   c_wr<= '1';
+				z_wr<= '1';
+			elsif (opcode = "000110" and ex2ma_c ='1') then 
+				c_wr<= '1';
+				z_wr<= '1';
+			elsif (opcode = "000101" and ex2ma_z ='1') then 
+				c_wr<= '1';
+				z_wr<= '1';
+			elsif (opcode = "001000" or opcode = "001011" ) then 
+				c_wr<= '0';
+				z_wr<= '1';
+			elsif (opcode = "001010" and ex2ma_c ='1') then 
+				c_wr<= '0';
+				z_wr<= '1';
+			elsif (opcode = "001001" and ex2ma_z ='1') then 
+				c_wr<= '0';
+				z_wr<= '1';
+			else 
+			   c_wr<= '0';
+				z_wr<= '0';
+			end if;
+			
+		end process;
+end architecture blackboxed3;
