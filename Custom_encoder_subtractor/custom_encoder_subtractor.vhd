0d1fdf29a3478eaa775819b98700b859daf773fb
shobman17 <210070081@iitb.ac.in>
2023-05-03 01:20:13 +0530

    modify encoder

--- Custom_encoder_subtractor/custom_encoder_subtractor.vhd
+++ Custom_encoder_subtractor/custom_encoder_subtractor.vhd
@@ -24,7 +24,7 @@ use ieee.std_logic_1164.all;
 entity custom_encoder is
 	port (input: in std_logic_vector(7 downto 0);
 			output: out std_logic_vector(2 downto 0);
-			done: out std_logic); -- nor of all input bits. Useful to know end of LM/SM instruction
+			all_zero, single_one: out std_logic); -- nor of all input bits. Useful to know end of LM/SM instruction
 end entity custom_encoder;
 
 architecture enc of custom_encoder is 
@@ -32,7 +32,21 @@ begin
 	output(2) <= input(0) or input(1) or input(2) or input(3);
 	output(1) <= input(0) or input(1) or (not(input(2) or input(3)) and (input(4) or input(5)));
 	output(0) <= input(0) or (not(input(1)) and input(2)) or (not(input(1) or input(3)) and input(4)) or (not(input(1) or input(3) or input(5)) and input(6));
-	done <= not(input(0) or input(1) or input(2) or input(3) or input(4) or input(5) or input(6) or input(7));
+	--all_zero <= not(input(0) or input(1) or input(2) or input(3) or input(4) or input(5) or input(6) or input(7));
+	with input select
+		single_one <= '1' when "00000001",
+				 '1' when "00000010",
+				 '1' when "00000100",
+				 '1' when "00001000",
+				 '1' when "00010000",
+				 '1' when "00100000",
+				 '1' when "01000000",
+				 '1' when "10000000",
+				 '0' when others;
+				 
+	with input select
+		all_zero <= '1' when "00000000",
+						'0' when others;
 end enc;
 
 library ieee;
